df759eabc99f10eb6a0e1db23667913bcb264c9f
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-02-02 22:14:22 +0530

    array example 1 added

---
+++ ArrayEx1.java
@@ -0,0 +1,14 @@
+public class ArrayEx1 {
+    public static void main(String[] args){
+        int arr1[] = new int[5];
+        arr1[0] = 1;
+        arr1[1] = 2; 
+        arr1[2] = 3;
+        arr1[3] = 4;
+        arr1[4] = 5;
+
+        for (int i = 0; i < arr1.length; i++ ){
+            System.out.println("Element at index " + i + " is : " + arr1[i]);
+        }
+    }
+}
