43b2b8a4b4cc348e1f03ed5d6972c5e9b13df335
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-02-02 21:56:34 +0530

    array basic details added

---
+++ array.java
@@ -0,0 +1,16 @@
+public class array {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5}; //BOth are valid array declarations
+
+        int arr1[] = {6,7,8,9,10};  // BOth are valid array declarations 
+
+        System.out.println(arr[0]);
+        System.out.println(arr1[0]);
+
+        int arr3[]; // declaring array
+        arr3 = new int[5];  // allocating memory to array
+
+        // or we can combine above both steps together and write in a single line.
+        int arr4[] = new int[5]; //eg
+    }
+}
