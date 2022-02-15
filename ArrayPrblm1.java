bd759797e2629d57581cdb53033bfd177b64a775
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-02-15 10:23:48 +0530

    problem solved

---
+++ ArrayPrblm1.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class ArrayPrblm1 {
+
+    public static int[] myfun(int[] arr, int length, int sum){
+        for (int i = 0; i < length - 1; i++){
+           for (int j = i+1; j < length; j++){
+               if (arr[i] + arr[j] == sum){
+                //    System.out.println("Two numbers are : " + arr[i] + ", " + arr[j]);
+                return new int[]{arr[i], arr[j]};
+               }
+           }
+        }
+        return new int[0];
+    }
+    public static void main(String[] args) {
+        int[] arr = {6, 5, 4, 3, 9, 8, 0};
+        // System.out.println(Arrays.toString(arr));  // This is how we can print array elements without using for loop.
+        int sum = 10;
+        int length = arr.length;
+
+        int[] result = myfun(arr, length, sum);
+        
+        System.out.println(Arrays.toString(result));
+    }
+}
