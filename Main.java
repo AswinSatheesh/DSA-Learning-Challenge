9a27ddb5e514cc834c5ae86505662b6d1d8df02f
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-02-22 22:08:25 +0530

    mcq questions created

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 
+//Showing that if we try to access an array element (length 5 and you try to access 6) then it will show "ArrayIndexOutOfBoundsException" error:
 public class Main {
     public static void main(String[] args) {
-        int[] Array = new int[5];                                                                
-        Array[5] = 7;
+        // int[] Array = new int[5];                                                                
+        // Array[5] = 7;
+        // int[] arr = (10,20,30,40,50);
+        int[] Arr = {10,20,30,40,50};
+        System.out.println(Arrays.toString(Arr));
     }
     
 }
