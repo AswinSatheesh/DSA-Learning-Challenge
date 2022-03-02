847b58d03f8a22610b6d4c316be3137418df5c05
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-03-02 22:51:47 +0530

    doubly linkedlist created and values added

---
+++ DoublyLinkedList/DLinkedList.java
@@ -0,0 +1,61 @@
+package DoublyLinkedList;
+
+public class DLinkedList {  //This is how doubly LinkedList create and add data to the nodes.
+   class Node{
+    int data;
+    Node next;
+    Node prev;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+    Node head, tail = null;
+
+    public void addElem(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = tail = newNode;
+            head.prev = null;
+            tail.next = null;
+        }
+        else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            tail.next = null;
+        }
+    }
+
+    public void display(){
+        Node current = head;
+        if(head == null){
+            System.out.println("List is empty.");
+            return;
+        }
+
+        while(current != null){
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+    }
+   
+
+
+   public static void main(String[] args) {
+        DLinkedList Dlist = new DLinkedList();
+        Dlist.addElem(5);
+        Dlist.addElem(6);
+        Dlist.addElem(4);
+        Dlist.addElem(2);
+       
+       System.out.println("Original linkedList is : ");
+
+       Dlist.display();
+       
+   }
+
+    
+}
