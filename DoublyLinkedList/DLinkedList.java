6852c36895594300d99dac9a53f48e614ac6feb1
AswinSatheesh <aswinsatheesh2001@gmail.com>
2022-03-05 07:43:13 +0530

    added new node at the end

--- DoublyLinkedList/DLinkedList.java
+++ DoublyLinkedList/DLinkedList.java
@@ -29,7 +29,7 @@ public class DLinkedList {  //This is how doubly LinkedList create and add data 
         }
     }
 
-    public void AddNodeInBeginning(int data){
+    public void AddNodeInBeginning(int data){ //This is how wer add node at the beginning.
         Node newNode = new Node(data);
 
         if(head == null){
@@ -43,6 +43,23 @@ public class DLinkedList {  //This is how doubly LinkedList create and add data 
             newNode.next = head;
             newNode.prev =  null;
             head = newNode;
+        }
+    }
+
+    public void AddNodeAtEnd(int data){ // This is how we add node at the end of a doubly linked list.
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+            tail = newNode;
+            head.prev = null;
+            tail.next = null;
+        }
+        else{
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+            tail.next = null;
         }
     }
 
@@ -74,8 +91,14 @@ public class DLinkedList {  //This is how doubly LinkedList create and add data 
 
        System.out.println();
 
-       System.out.println("New node added at the beginning.");
+       System.out.println("New node added (60) at the beginning.");
        Dlist.AddNodeInBeginning(60);
+       Dlist.display();
+
+       System.out.println();  //creating a space between the lines
+
+       System.out.println("New node added (90) at the end.");
+       Dlist.AddNodeAtEnd(90);
        Dlist.display();
        
    }
